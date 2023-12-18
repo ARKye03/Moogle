0cc3fb9808c0669f33ddfda0718ffac767302222
ARKye03 <rafa2003sanchez@gmail.com>
2023-12-18 13:30:43 -0500

    Start commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className='text-blue-400 hover:text-black duration-200 transition'>Hello bro</h1>
+        
+      </div>
+    </>
+  )
+}
+
+export default App
